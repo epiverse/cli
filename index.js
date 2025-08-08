1a0dcf4648cc35dd69fffa5ff77c98d4349483f8
Jonas Almeida <jalmeida@mathbiol.org>
2025-08-08 09:29:03 -0400

    ini

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(`index.js ran at ${Date()}`)
+
+(async function(){
+    //lala = await import('../gpt/min.js')
+    caterpillar = await import('./caterpillar.js')
+    //console.log('hello')
+})();
