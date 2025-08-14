4ebb9276acca31fe07c19d7769c408bc99b03ba4
Jonas Almeida <jalmeida@mathbiol.org>
2025-08-14 16:39:59 -0400

    own methodos

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ console.log(`index.js at ${Date()}`);
 
 (async function(){ // anonymous asynchronous execution
     let cli = await import('./cli.mjs')
-    cli.msg()
+    cli.msg() // opening message
 })();
-
-//const help=9
-//export {help}
