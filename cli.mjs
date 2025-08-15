b0fe8021b9fe62aac40f410aa56b46ee2b3c9cc0
Jonas Almeida <jalmeida@mathbiol.org>
2025-08-15 13:18:35 -0400

    unzipURL

--- cli.mjs
+++ cli.mjs
@@ -12,8 +12,30 @@ async function msg(txt=`${Date()}`,div='cliDiv'){
         div.innerText=txt.slice(0,i)
     }
 }
+
 function help(){
     window.open('https://github.com/epiverse/cli/wiki/CLI')
 }
 
-export{msg,help}
+async function unzipURL(url="https://raw.githubusercontent.com/epiverse/pathembed/refs/heads/main/tcgaSlideEmbeddings.json.zip") {
+    //const localForage = await import("https://esm.sh/localforage")
+    const JSZip = (await import('https://esm.sh/jszip@3.10.1')).default
+    // xx = await (await import('./embedStats.mjs')).unzipURL()
+    console.log(`unzipping from ${url}\nthis may take a few seconds, maybe a minute ...`)
+    msg.value = `unzipping from ${url}\nthis may take a few seconds, maybe a minute ...`
+    let response = await fetch(url)
+    let data = await response.arrayBuffer()
+    let zip = await JSZip.loadAsync(data);
+    let filename = Object.entries(zip.files)[0][0]
+    let file = zip.file(filename)
+    let content = await file.async('string')
+    if (content.match(/^[\[\{]/)) {
+        // text content starts with '[' or '{'
+        content = await JSON.parse(content)
+        console.log('JSON text detected, ... parsed:')
+    }
+    return content
+    console.log(`... ${content.length} embedded vectors loaded and unzipped`)
+}
+
+export{msg,help,unzipURL}
